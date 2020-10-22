8354381e99889278a12f66b9e411c22532b72216
RagdIR <ragdirstrogov@gmail.com>
2020-10-22 19:52:41 +0600

    Готовое начало

--- source/main/urls.py
+++ source/main/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from main import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/', include('api.urls')),
+    path('', include('webapp.urls')),
+    path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
